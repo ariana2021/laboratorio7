9892fe16913a3d9752fecee8fe2f762194bf80c2
ariana2021 <ariana.azabache@tecsup.edu.pe>
2023-10-14 23:47:57 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio7/laboratorio7/Estudiantes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laboratorio7
+{
+    public class Estudiantes
+    {
+        public string Name {  get; set; }
+        public string Ciclo { get; set; }
+        public string seccion {  get; set; }
+        public string Edad { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ laboratorio7/laboratorio7/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace laboratorio7
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Estudiantes> Estudiantes { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Estudiantes = new List<Estudiantes>();
+
+            Estudiantes.Add(new Estudiantes
+            {
+                Name = "Ariana",
+                Ciclo = "5",
+                seccion = "A",
+                Edad = "20"
+            });
+
+            Estudiantes.Add(new Estudiantes
+            {
+                Name = "Nicol",
+                Ciclo = "5",
+                seccion = "A",
+                Edad = "20"
+            });
+
+            Estudiantes.Add(new Estudiantes
+            {
+                Name = "Jose",
+                Ciclo = "5",
+                seccion = "A",
+                Edad = "20"
+            });
+            BindingContext = this;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+    }
+}
